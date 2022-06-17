58c0a133d2507c9bd04a3185a8fc0613689ab5c4
CNED-SLAM <emds@bodhissatva.com>
2022-06-17 17:09:58 +0200

    Phase 19 : gestion des images

--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -2,11 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Environnement;
 use App\Entity\Visite;
 use DateTime;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -33,10 +35,14 @@ class VisiteType extends AbstractType
                 'label' => 't° max'
             ])
             ->add('environnements', EntityType::class, [
-                'class' => \App\Entity\Environnement::class,
+                'class' => Environnement::class,
                 'choice_label' => 'nom',
                 'multiple' => true,
                 'required' => false
+            ])
+            ->add('imageFile', FileType::class, [
+                'required' => false,
+                'label' => 'sélection image'
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Enregistrer'
