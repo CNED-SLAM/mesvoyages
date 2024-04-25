a5780ab82e22705bdcd40e8355bc78fba2f4ba94
EmdsTest <cned@bodhissatva.com>
2024-04-25 14:25:12 +0200

    Phase 23 : tests (compléments)

--- tests/VisiteTest.php
+++ tests/VisiteTest.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Tests;
 
+use App\Entity\Environnement;
 use App\Entity\Visite;
+use DateTime;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -13,8 +15,19 @@ class VisiteTest extends TestCase {
 
     public function testGetDatecreationString(){
         $visite = new Visite();
-        $visite->setDatecreation(new \DateTime("2024-04-24"));
+        $visite->setDatecreation(new DateTime("2024-04-24"));
         $this->assertEquals("24/04/2024", $visite->getDatecreationString());
+    }
+    
+        public function testAddEnvironnement(){
+        $environnement = new Environnement();
+        $environnement->setNom("plage");
+        $visite = new Visite();
+        $visite->addEnvironnement($environnement);
+        $nbEnvironnementAvant = $visite->getEnvironnements()->count();
+        $visite->addEnvironnement($environnement);
+        $nbEnvironnementApres = $visite->getEnvironnements()->count();
+        $this->assertEquals($nbEnvironnementAvant, $nbEnvironnementApres, "ajout même environnement devrait échouer");
     }
     
 }
